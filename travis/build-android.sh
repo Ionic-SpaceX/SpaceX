7bea2fa8324c6fef26754ee3e0af662a25ba047d
CNadjim <c.nadjim@gmail.com>
2018-05-25 14:04:01 +0200

    test travis

--- travis/build-android.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -v
-set -e
-# Build Ionic App for Android
-ionic cordova platform add android --nofetch
-ionic cordova build android
-
--- travis/package-android.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -v
-
-set -e
-
-mkdir -p output
-cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk output/app-release-unsigned.apk
-
