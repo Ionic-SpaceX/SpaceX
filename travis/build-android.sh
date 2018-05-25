00c141083dc62c3dba7cfd377ffd2ab435b82873
CNadjim <c.nadjim@gmail.com>
2018-05-25 11:27:11 +0200

    test travis

--- travis/build-android.sh
+++ travis/build-android.sh
@@ -4,10 +4,5 @@ set -e
 
 # Build Ionic App for Android
 cordova platform add android --nofetch
+ionic cordova build android
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]
-then
-    ionic cordova build android
-else
-    ionic cordova build android --prod --release
-fi
--- travis/package-android.sh
+++ travis/package-android.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]
-then
-    echo "Skipping package Android for develop branch"
-    exit
-fi
-
 mkdir -p output
 cp platforms/android/build/outputs/apk/android-release-unsigned.apk output/ionictravis-release-unsigned.apk
 
