242f5ebe182caad88038a74873c28d3c65fadba3
gurnav <singh.g@outlook.fr>
2018-06-25 18:22:46 +0200

    added authenticate method

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  fire: {
+    apiKey: "AIzaSyC2fh6l42TmwP2AyHGCViraiBWhHip2jUw",
+    authDomain: "esgispacex.firebaseapp.com",
+    databaseURL: "https://esgispacex.firebaseio.com",
+    projectId: "esgispacex",
+    storageBucket: "esgispacex.appspot.com",
+    messagingSenderId: "321576493316"
+  }
+};
