294dd5e598e57d39bef62240c8ca7eeebe5a1257
CNadjim <c.nadjim@gmail.com>
2018-06-29 19:53:31 +0200

    custom sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import {Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
@@ -8,7 +8,6 @@ import { CapsulesPage } from '../pages/capsules/capsules';
 import { LaunchesPage } from '../pages/launches/launches';
 import {CacheService} from "ionic-cache";
 import {SettingsPage} from "../pages/settings/settings";
-import { LoginPage } from '../pages/login/login';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
@@ -19,14 +18,17 @@ export class MyApp {
   rootPage;
   isUser: boolean = false;
   private platform;
-
+  public activePage:any;
   @ViewChild(Nav) nav: Nav;
-    pages: Array<{title: string, component: any, icon: any}>;
+  pages: Array<{title: string, component: any, icon: any}>;
+
     constructor(platform: Platform,private statusBar: StatusBar, private aFauth: AngularFireAuth, private cache: CacheService) {
     this.platform = platform;
     this.initializeApp();
+    this.rootPage = HomePage;
+    this.activePage = HomePage;
     this.cache.setDefaultTTL(60 * 60);
-
+/*
     aFauth.authState.subscribe(user => {
       if(user) {
         this.rootPage = HomePage;
@@ -36,7 +38,7 @@ export class MyApp {
       }
     })
 
-    this.isUserConnected();
+    this.isUserConnected();*/
     this.pages = [
       { title: 'Home', component: HomePage, icon: 'home' },
       { title: 'About', component: AboutPage, icon: 'information-circle' },
@@ -55,6 +57,10 @@ export class MyApp {
 
   openPage(page) {
     this.nav.setRoot(page.component);
+    this.activePage = page;
+  }
+  isActivePage(page){
+      return this.activePage == page;
   }
 
   logout(){
@@ -71,4 +77,7 @@ export class MyApp {
       }
     })
   }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import {SettingsPageModule} from "../pages/settings/settings.module";
 
 import { LoginPageModule } from '../pages/login/login.module';
 import { RegisterPageModule } from '../pages/register/register.module';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialModule} from "./material.module";
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { RegisterPageModule } from '../pages/register/register.module';
 
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     HttpClientModule,
     CapsulesPageModule,
     RocketsPageModule,
